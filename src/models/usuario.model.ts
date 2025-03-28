9c7b96fdef9e2738c7809fe6371a0021eb252a91
anaca2804 <anacarolinaf2804@gmail.com>
2025-03-27 21:45:08 -0300

    wip: criando as models de permissao e usuario

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+interface IUsuario extends mongoose.Document{
+    nome: string;
+    email: string;
+    senha: string;
+};
+
+const UsuarioSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    senha: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+const Usuario = mongoose.model<IUsuario>('Usuario', UsuarioSchema);
+
+export{
+    Usuario,
+    UsuarioSchema,
+    IUsuario
+}
