a03d226b914df669654bce6582c5aa560ee184ef
Your Name <you@example.com>
2025-03-31 21:29:28 -0300

    wip: iniciando cadastro de rotas

---
+++ src/main.router.ts
@@ -0,0 +1,16 @@
+import * as express from "express";
+import { Router } from "./common/router";
+
+const { name, version, description} = require('../package.json')
+
+class MainRouter extends Router {
+    applyrouter(application: express.Application){
+        application.get('/', (req, resp, next) => {
+            resp.json({
+                name,
+                version,
+                description
+            })
+        })
+    }
+}
