c665b0da7ea225651c77c4c8a0ecc99018ce8323
Gabriel Costa <Gabriel.neri2@hotmail.com>
2025-04-25 21:14:27 -0300

    Criando Controllers

---
+++ src/controllers/produto.controller.ts
@@ -0,0 +1,23 @@
+import { ModelRouter } from "../common/model-router";
+import * as express from 'express'
+import { IProduto, Produto } from "../models/produto.models"; 
+
+class ProdutoController extends ModelRouter<IProduto>{
+    constructor() {
+        super(Produto)
+    }
+    applyrouter(app: express.Application) {
+        app.get(`${this.basePath}`, this.find);
+        app.get(`${this.basePath}/:id`, [this.validateID,this.findById]);
+        app.post(`${this.basePath}`, this.save);
+        app.patch(`${this.basePath}/:id`, [this.validateID,this.update]);
+        app.put(`${this.basePath}/:id`, [this.validateID,this.replace]);
+        app.delete(`${this.basePath}`, [this.validateID,this.delete]);
+    }
+};
+
+const ProdutosController = new ProdutoController();
+
+console.log(ProdutosController.basePath);
+
+export {ProdutosController};
