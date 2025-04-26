f50b77cb43e57fcc5331fa026012b5836febcf5e
João Aiub <joaoribeiroaiub@gmail.com>
2025-04-26 19:03:49 -0300

    feat: controllers e rotas
    Criando os metodos de intereção com as collectios, controllers
    associadas a cada uma das collections e adicionando as mesmas ao app
    express.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { LogsController } from "./controllers/log.controller";
+import { MovimentacoesController } from "./controllers/movimentacoes-estoque.controller";
+import { PermissaoCadController } from "./controllers/permissao.controller";
+import { PermissoesMoveController } from "./controllers/permissoes.controller";
+import { ProdutosController } from "./controllers/produto.controller";
 import { UsuariosController } from "./controllers/usuario.controller";
 import { mainRouter } from "./main.router";
 import { Server } from "./server/server"; 
@@ -9,5 +13,9 @@ const aplicacao = new Server()
 aplicacao.iniciarServico([
     mainRouter,
     LogsController,
+    MovimentacoesController,
+    PermissaoCadController,
+    PermissoesMoveController,
+    ProdutosController,
     UsuariosController
 ])
