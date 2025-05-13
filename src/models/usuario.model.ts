f31e33448c4281c96499a094b72d6d947dbbfd95
luana-eb <luana.eb@hotmail.com>
2025-05-12 21:44:32 -0300

    wip: trabalhando na criptografia

--- src/models/usuario.model.ts
+++ src/models/usuario.model.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import * as bcrypt from "bcrypt";
 interface IUsuario extends mongoose.Document{
     nome: string;
     email: string;
@@ -23,6 +23,24 @@ const UsuarioSchema = new mongoose.Schema({
 
 const Usuario = mongoose.model<IUsuario>('Usuario', UsuarioSchema);
 
+const encriptaSenha = (obj, next) => {
+    bcrypt
+        .hash(obj.senha, 10)
+        .then((hash)=> {
+            obj.senha = hash;
+            next();
+        })
+        .catch(next);
+};
+
+const saveMiddleware = function (this: IUsuario, next) {
+    const usuario: IUsuario = this;
+    if (!usuario.isModified("senha")) {
+        next();
+    }else {
+        encriptaSenha(usuario,next);
+    }
+}
 export{
     Usuario,
     UsuarioSchema,
