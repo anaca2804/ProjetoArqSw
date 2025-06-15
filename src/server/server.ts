3fafacec44ec4f4141b5d23758ef67ec5d80fde5
Gabriel Neri e Costa <102746108+GabrielNeri2@users.noreply.github.com>
2025-06-15 14:06:32 -0300

    style(server)
    Correção de digitação, tipagem e melhora no tratamento de erro

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,44 +1,44 @@
 import express, { Express } from "express";
-import { enviroment } from "../common/environment";
+import { environment } from "../common/environment";
 import mongoose from "mongoose";
 import { Router } from "../common/router";
 import { handleError } from "./error.handler";
 
-const port = enviroment.server.port;
+const port = environment.server.port;
 
 export class Server {
-    app: Express = express();
+  app: Express = express();
 
-    constructor() {
-        this.app.use(express.json());
+  constructor() {
+    this.app.use(express.json());
+  }
+
+  iniciarServidor = (): void => {
+    this.app.listen(port, () => {
+      console.log(`Servidor rodando em http://localhost:${port}`);
+    });
+  };
+
+  async conectarBaseDados(): Promise<void> {
+    try {
+      await mongoose.connect(environment.db.url);
+      console.log("Conectado ao banco");
+    } catch (error) {
+      throw new Error(`Erro: ${error}`);
     }
+  }
 
-    iniciarServidor = () => {
-        this.app.listen(port, () => {
-            console.log(`Servidor rodando em http://localhost:${port}`);
-        });
-    };
+  iniciarRotas = (routers: Router[]): void => {
+    routers.forEach((router) => router.applyrouter(this.app));
+    this.app.use(handleError);
+  };
 
-    async conectarBaseDados() {
-        try {
-            await mongoose.connect(enviroment.db.url);
-            console.log("Conectado ao banco");
-        } catch (error) {
-            throw (`Erro: ${error}`);
-        }
-    }
-
-    iniciarRotas = (routers: Router[]) => {
-        routers.forEach(router => router.applyrouter(this.app));
-        this.app.use(handleError);
-    }
-
-    iniciarServico = (routers: Router[]): Promise<Server> => {
-        return this.conectarBaseDados()
-            .then(() => this.iniciarRotas(routers))
-            .then(() => {
-                this.iniciarServidor();
-                return this;
-            });
-    }
+  iniciarServico = (routers: Router[]): Promise<Server> => {
+    return this.conectarBaseDados()
+      .then(() => this.iniciarRotas(routers))
+      .then(() => {
+        this.iniciarServidor();
+        return this;
+      });
+  };
 }
