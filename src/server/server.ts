1a0c78d71d926f0dd7d188aa108ba2ca4081b87d
GabrielNeri2 <gabriel.neri2@hotmail.com>
2025-06-15 19:13:09 -0300

    Revert "style(server)"
    This reverts commit 3fafacec44ec4f4141b5d23758ef67ec5d80fde5.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,44 +1,44 @@
 import express, { Express } from "express";
-import { environment } from "../common/environment";
+import { enviroment } from "../common/environment";
 import mongoose from "mongoose";
 import { Router } from "../common/router";
 import { handleError } from "./error.handler";
 
-const port = environment.server.port;
+const port = enviroment.server.port;
 
 export class Server {
-  app: Express = express();
+    app: Express = express();
 
-  constructor() {
-    this.app.use(express.json());
-  }
-
-  iniciarServidor = (): void => {
-    this.app.listen(port, () => {
-      console.log(`Servidor rodando em http://localhost:${port}`);
-    });
-  };
-
-  async conectarBaseDados(): Promise<void> {
-    try {
-      await mongoose.connect(environment.db.url);
-      console.log("Conectado ao banco");
-    } catch (error) {
-      throw new Error(`Erro: ${error}`);
+    constructor() {
+        this.app.use(express.json());
     }
-  }
 
-  iniciarRotas = (routers: Router[]): void => {
-    routers.forEach((router) => router.applyrouter(this.app));
-    this.app.use(handleError);
-  };
+    iniciarServidor = () => {
+        this.app.listen(port, () => {
+            console.log(`Servidor rodando em http://localhost:${port}`);
+        });
+    };
 
-  iniciarServico = (routers: Router[]): Promise<Server> => {
-    return this.conectarBaseDados()
-      .then(() => this.iniciarRotas(routers))
-      .then(() => {
-        this.iniciarServidor();
-        return this;
-      });
-  };
+    async conectarBaseDados() {
+        try {
+            await mongoose.connect(enviroment.db.url);
+            console.log("Conectado ao banco");
+        } catch (error) {
+            throw (`Erro: ${error}`);
+        }
+    }
+
+    iniciarRotas = (routers: Router[]) => {
+        routers.forEach(router => router.applyrouter(this.app));
+        this.app.use(handleError);
+    }
+
+    iniciarServico = (routers: Router[]): Promise<Server> => {
+        return this.conectarBaseDados()
+            .then(() => this.iniciarRotas(routers))
+            .then(() => {
+                this.iniciarServidor();
+                return this;
+            });
+    }
 }
